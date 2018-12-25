babb905846b1d2241691c2902f4a8ba98339e9ea
Andrew Meier <ameier38@gmail.com>
2018-12-25 16:20:54 -0500

    update consumer; remove old producer

--- Consumer.fs
+++ Consumer.fs
@@ -1,20 +1,28 @@
 module Consumer
 
+open System.Text
 open Confluent.Kafka
+
+let bytesToString (bytes:byte []) =
+    Encoding.UTF8.GetString bytes
 
 let runConsumer (broker:string) (topic:string) (groupId:string) =
     let config =
         Config.Consumer.safe
         |> Config.bootstrapServers broker
         |> Config.Consumer.groupId groupId
+        |> Config.Consumer.Topic.autoOffsetReset Config.Consumer.Topic.Beginning
         |> Config.clientId "test-client"
 
     use consumer = Consumer.create config
 
     let handle (messageSet:ConsumerMessageSet) =
         async {
-            printfn "%A" messageSet.messages
+            messageSet.messages
+            |> Seq.iter (fun msg -> msg.Value |> bytesToString |> printfn "message: %s")
         }
 
-    Consumer.consume consumer 1000 1000 1 handle
+    consumer.Subscribe topic
+
+    Consumer.consume consumer 1000 1000 10 handle
     |> Async.RunSynchronously
